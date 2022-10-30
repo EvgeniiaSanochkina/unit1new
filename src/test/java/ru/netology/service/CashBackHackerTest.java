8a190943603b68d30f8cd14b7b05ea2c0dd97fd7
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-10-30 14:02:49 +0700

    testingDone

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -20,4 +20,11 @@ public class CashBackHackerTest {
         int expected = 500;
         assertEquals(actual, expected);
     }
+    @Test
+    public void CashBackIfEqualsBoundary(){
+        CashBackHacker service = new CashBackHacker();
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
 }
