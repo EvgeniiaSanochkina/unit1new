3542c5217756cf8eb29583dc79a2fea15b4313ab
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-10-30 13:47:59 +0700

    junit4Done

--- src/main/java/ru/netology/service/CashBackHacker.java
+++ src/main/java/ru/netology/service/CashBackHacker.java
@@ -3,6 +3,10 @@ package ru.netology.service;
 public class CashBackHacker {private final int boundary = 1000;
 
     public int remain(int amount) {
-        return boundary - amount % boundary;
+        if (amount == 1000) {
+            return 0;
+        } else {
+            return boundary - amount % boundary;
+        }
     }
 }
---
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -0,0 +1,32 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashBackHackerTest {
+    @Test
+    public void CashBackIfLessThanBoundary(){
+        CashBackHacker service = new CashBackHacker();
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void CashBackIfMoreThanBoundary(){
+        CashBackHacker service = new CashBackHacker();
+        int actual = service.remain(2500);
+        int expected = 500;
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void CashBackIfEqualsBoundary(){
+        CashBackHacker service = new CashBackHacker();
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+}
+
+
