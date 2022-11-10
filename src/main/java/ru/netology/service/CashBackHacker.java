543329536ff7ccd4956aa05a2e109a7df1ba2d44
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-11-10 23:24:20 +0700

    fixed

--- src/main/java/ru/netology/service/CashBackHacker.java
+++ src/main/java/ru/netology/service/CashBackHacker.java
@@ -3,10 +3,6 @@ package ru.netology.service;
 public class CashBackHacker {private final int boundary = 1000;
 
     public int remain(int amount) {
-        if (amount == 1000) {
-            return 0;
-        } else {
-            return boundary - amount % boundary;
-        }
+        return boundary - amount % boundary;
     }
 }
